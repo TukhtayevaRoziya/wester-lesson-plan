e093ea6dc22bd75baae52bb58953bccd001eb439
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2025-01-28 18:02:29 +0500

    homework

--- fetch/main.js
+++ fetch/main.js
@@ -10,14 +10,20 @@
 // // sdfsdf
 // // })
 
-fetch("https://reqres.in/api/users/23", {
+fetch("https://fakestoreapi.com/products", {
   method: "POST",
 //   CRUD 
   headers: {
     "Content-Type": "application/json",
   },
   body: JSON.stringify({
-    name: "User 1",
+    title: "User 1",
+    price: 34,
+    description: 'dsfsdfsdfsdf',
+    image:'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+    rating:{
+      rate: 4.3
+    }
   }),
 })
   .then((res) => {
@@ -31,6 +37,12 @@ fetch("https://reqres.in/api/users/23", {
 
   .then((data) => {
     console.log(data);
+    
+    document.getElementById('myImage').src = data?.image
+    document.getElementById('title').innerHTML ='' + data?.title
+    document.getElementById('my_desc').innerHTML = 'Description: ' + data?.description
+    document.getElementById('myId').innerHTML = 'ID: ' + data?.id
+    document.getElementById('my_price').innerHTML = 'Price: ' + data?.price + " so'm"
   })
 
   .catch((err) => console.log("error"));
